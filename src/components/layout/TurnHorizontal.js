926c7eeaba2c148022c1bbbc636c9b41003283ac
viniponteslima <1999vini1999@gmail.com>
2025-12-11 16:27:57 -0300

    refactor: remover conteúdo desnecessário no reset.css e adicionar título ao SVG em TurnHorizontal

--- src/components/layout/TurnHorizontal.js
+++ src/components/layout/TurnHorizontal.js
@@ -25,6 +25,7 @@ export default function TurnHorizontal() {
 				<div className={styles.turnHorizontal}>
 					<span>Vire o seu celular para horizontal</span>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<title>Turn Horizontal</title>
 						<path d="M17,19H7V5H17M17,1H7C5.89,1 5,1.89 5,3V21A2,2 0 0,0 7,23H17A2,2 0 0,0 19,21V3C19,1.89 18.1,1 17,1Z" />
 					</svg>
 				</div>
